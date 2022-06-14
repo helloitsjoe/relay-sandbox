784767dee38c215d3bfe76d16f8cf641bd78a7ed
helloitsjoe <jboyle617@gmail.com>
2022-06-13 20:19:53 -0400

    Update posts

---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,134 @@
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import type { Post } from "~/models/post.server";
+import { getPost, updatePost } from "~/models/post.server";
+
+type LoaderData = { post: Post };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, "params.slug is required");
+
+  const post = await getPost(params.slug);
+  invariant(post, "Post not found");
+
+  return json<LoaderData>({ post });
+};
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export const action: ActionFunction = async ({ request }) => {
+  // TODO: Remove me
+  await new Promise((res) => setTimeout(res, 1000));
+
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors: ActionData = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+
+  const hasErrors = Object.values(errors).some((msg) => msg);
+
+  if (hasErrors) {
+    return json<ActionData>(errors);
+  }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
+  await updatePost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+export default function NewPost() {
+  const errors = useActionData();
+  const {
+    post: { title, slug, markdown },
+  } = useLoaderData();
+  console.log("markdown", markdown);
+
+  const transition = useTransition();
+  const isCreating = !!transition.submission;
+
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
+          <input
+            type="text"
+            name="title"
+            className="inputClassName"
+            defaultValue={title}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input
+            type="text"
+            name="slug"
+            className="inputClassName"
+            defaultValue={slug}
+          />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+        </label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={10}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+          // Why isn't this updating?
+          defaultValue={markdown}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
+        >
+          {isCreating ? "Updating..." : "Update Post"}
+        </button>
+      </p>
+    </Form>
+  );
+}
